6403509d81c25237aa302eab13dac78d7cfda8b3
smammc <smammc@hotmail.com>
2024-07-23 15:24:32 +0100

    removed extensions of card class and added functions in card class

--- card.js
+++ card.js
@@ -1,14 +1,6 @@
 class Card {
-  constructor(
-    name,
-    attack,
-    defense,
-    gameScreen,
-    width,
-    height,
-    imgSrc,
-    health
-  ) {
+  //prettier-ignore
+  constructor(name, attack, defense, gameScreen, width, height, imgSrc, health) {
     this.name = name;
     this.attack = attack;
     this.defense = defense;
@@ -27,5 +19,28 @@ class Card {
     this.element.width = `${width}px`;
     this.element.height = `${height}px`;
     this.gameScreen.appendChild(this.element);
+
+    let enemyUnit = document.getElementById("enemy-div");
+    updateCurrentHealth();
+  }
+
+  updateCurrentHealth() {
+    const enemyCurrentHealth = document.getElementById("enemyCurrentHealth");
+    enemyCurrentHealth.innerHTML = this.health;
+  }
+
+  getHealth() {
+    // Return the value of health at the moment
+    return this.health;
+  }
+
+  decreaseHealth(amount) {
+    // Reduce the value of health by the amount given
+    this.health -= amount;
+  }
+
+  getAttack() {
+    // return the value of attack
+    return this.attack;
   }
 }
